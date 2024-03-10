103458c65c02ff4bcaf97207a5c075e398463104
Mendo6472 <mendo6472@gmail.com>
2024-03-10 15:02:52 -0500

    WIP:BUILD:Added contract request model

--- hiring_module/hiring_app/models.py
+++ hiring_module/hiring_app/models.py
@@ -77,3 +77,84 @@ class ApplicationTemplateManager(models.Manager):
 
         application_template.save(using=self._db)
         return application_template
+    
+class ContractRequestManager(models.Manager):
+    def create_contract_request(self, created_by, template, user_information=None, estimated_completion_date=None):
+        if not created_by:
+            raise ValueError('The Contract Request must have a creator')
+        if not template:
+            raise ValueError('The Contract Request must have a template')
+        contract_request = self.model(
+            created_by=created_by,
+            template=template,
+            current_state_start=timezone.now(),
+            estimated_completion_date=estimated_completion_date,
+            user_information=user_information
+        )
+        contract_request.save(using=self._db)
+        return contract_request
+
+    
+class ContractRequestState(models.Model):
+    contract_request = models.ForeignKey('ContractRequest', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class ContractRequest(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    estimated_completion_date = models.DateTimeField()
+    current_state_start = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    template = models.ForeignKey(ApplicationTemplate, on_delete=models.CASCADE)
+    state = models.ForeignKey(ContractRequestState, on_delete=models.PROTECT)
+    user_information = models.FileField(upload_to=os.path.join(MEDIA_ROOT, 'user_information'), null=True, blank=True)
+    def __str__(self):
+        return str(self.id)
+
+    def transition_to_state(self, new_state, comment=None):
+        previous_state = self.state
+
+        self.state = new_state
+        self.current_state_start = timezone.now()
+        self.save()
+
+        ContractRequestSnapshot.objects.create(
+            contract_request=self,
+            state=str(previous_state),
+            state_start=self.current_state_start,
+            state_end=timezone.now(),
+            comment=comment
+        )
+
+        return previous_state 
+    
+    def get_snapshots(self):
+        return ContractRequestSnapshot.objects.filter(contract_request=self)
+
+class ContractRequestSnapshot(models.Model):
+    contract_request = models.ForeignKey(ContractRequest, on_delete=models.CASCADE)
+    state = models.CharField(max_length=64)
+    state_start = models.DateTimeField()
+    state_end = models.DateTimeField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return str(self.id)
+
+class ContractRequestReviewState(ContractRequestState):
+    def __str__(self):
+        return "Review"
+
+class ContractRequestIncompleteState(ContractRequestState):
+    def __str__(self):
+        return "Incomplete"
+
+class ContractRequestFiledState(ContractRequestState):
+    def __str__(self):
+        return "Filed"
+    
+class ContractRequestCancelledState(ContractRequestState):
+    def __str__(self):
+        return "Cancelled"
