1c843764848661a5dfb5d0596f7de3ec809811ce
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2024-03-17 23:39:44 -0500

    END: Refactor: tailwind

--- hiring_module/hiring_app/apps.py
+++ hiring_module/hiring_app/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class HiringAppConfig(AppConfig):
+class Hiring_apppConfig(AppConfig):    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hiring_app'
+
--- hiring_module/hiring_app/urls.py
+++ hiring_module/hiring_app/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 from hiring_app.views import ControlBoardView
 
 app_name = 'hiring_app'
 
 urlpatterns = [
     path('control_board/', ControlBoardView.as_view(), name = 'control_board',)
+
 ]
