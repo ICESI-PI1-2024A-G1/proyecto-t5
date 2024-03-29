a098f44ff57df5c043706f266b598348d936f904
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2024-03-29 00:06:55 -0500

    Hotfix

--- hiring_module/hiring_app/urls.py
+++ hiring_module/hiring_app/urls.py
@@ -1,9 +1,5 @@
 from django.urls import include, path
-<<<<<<< HEAD
-from hiring_app.views import ExternalUserDashboardView, AdministratorDashboardView, LeaderDashboardView, ManagerDashboardView, InfoView
-=======
 from hiring_app.views import *
->>>>>>> e23257135934b5973cbac37afe1f0fa65fbe0a23
 
 app_name = 'hiring_app'
 
@@ -12,12 +8,8 @@ urlpatterns = [
     path('administrator_dashboard/', AdministratorDashboardView.as_view(), name = 'administrator_dashboard'),
     path('leader_dashboard/', LeaderDashboardView.as_view(), name = 'leader_dashboard'),
     path('manager_dashboard/', ManagerDashboardView.as_view(), name = 'manager_dashboard'),
-<<<<<<< HEAD
-    path('info/<str:idContract>/', InfoView.as_view(), name='multiply_by_two'),
-=======
-    path('info/<str:idContract>/', ChangeState.as_view(), name='multiply_by_two'),
+    path('info/<str:idContract>/', InfoView.as_view(), name='InfoView'),
     path('request_creation/cex', CEXContractRequestView.as_view(), name = 'cex',),
     path('request_creation/monitoring', MonitoringContractRequestView.as_view(), name = 'monitoring',),
 
->>>>>>> e23257135934b5973cbac37afe1f0fa65fbe0a23
 ]
