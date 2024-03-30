3a2cf4df97a953ae80d2ae101033c38db43d875a
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2024-03-30 11:50:54 -0500

    update

---
+++ hiring_module/hiring_app/static/js/side_bar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleBtn = document.getElementById('toggleAside');
+    const sidebar = document.getElementById('asideMenu');
+    const mainContent = document.getElementById('main');
+
+    toggleBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('hidden');
+        mainContent.classList.toggle('expanded');
+        mainContent.classList.toggle('nonExpanded');
+
+    });
+});
