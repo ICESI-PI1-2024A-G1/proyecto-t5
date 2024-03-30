257714a1192ab195df6fe1bcfb3e3604b3675715
Mendo6472 <mendo6472@gmail.com>
2024-03-30 01:34:57 -0500

    END:BUILD: Added some default cex contracts and monitoring contracts

--- hiring_module/hiring_app/signals/user_database_inserts_signal.py
+++ hiring_module/hiring_app/signals/user_database_inserts_signal.py
@@ -1,4 +1,6 @@
 from hiring_app.model.user_model import CustomUser
+from hiring_app.model.cex_contract_request_model import CEXContractRequest
+from hiring_app.model.monitoring_contract_request_model import MonitoringContractRequest
 from django.contrib.auth.models import Group
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
@@ -10,8 +12,15 @@ import os
 DEFAULT_GROUPS = ['admin', 'leader', 'manager']
 
 default_users_path = os.path.join(settings.BASE_DIR, 'hiring_app','intial_data', 'users.json')
+default_cex_path = os.path.join(settings.BASE_DIR, 'hiring_app','intial_data', 'cex_contract_requests.json')
+default_monitoring_path = os.path.join(settings.BASE_DIR, 'hiring_app','intial_data', 'monitoring_contract_requests.json')
 
 DEFAULT_USERS = json.load(open(default_users_path))
+DEFAULT_CEX = json.load(open(default_cex_path))
+DEFAULT_MONITORING = json.load(open(default_monitoring_path))
+
+total_cex = len(DEFAULT_CEX)
+total_monitoring = len(DEFAULT_MONITORING)
 
 def create_default_groups():
     for group_name in DEFAULT_GROUPS:
@@ -24,6 +33,8 @@ def user_database_inserts_signal(sender, **kwargs):
     if sender != auth_config:
         return
     create_default_groups()
+    current_cex = 0
+    current_monitoring = 0
     for user_data in DEFAULT_USERS:
         user_id = user_data['id']
         if not CustomUser.objects.filter(id=user_id).exists():
@@ -44,3 +55,15 @@ def user_database_inserts_signal(sender, **kwargs):
                 if group_name:
                     group = Group.objects.get(name=group_name)
                     user.groups.add(group)
+            elif current_cex < total_cex:
+                cex = DEFAULT_CEX[current_cex]
+                cex['created_by'] = user
+                cex['rut'] = f"rut"
+                CEXContractRequest.objects.create(**cex)
+                current_cex += 1
+            elif current_monitoring < total_monitoring:
+                monitoring = DEFAULT_MONITORING[current_monitoring]
+                monitoring['created_by'] = user
+                MonitoringContractRequest.objects.create(**monitoring)
+                current_monitoring += 1
+            
