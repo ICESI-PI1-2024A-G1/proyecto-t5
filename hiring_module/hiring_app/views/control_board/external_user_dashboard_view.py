fdb1615917dcac76bca430092249d44e537d564c
Mendo6472 <mendo6472@gmail.com>
2024-03-31 12:11:42 -0500

    END:BUILD: Fixed going into the dashboard when not logged in

--- hiring_module/hiring_app/views/control_board/external_user_dashboard_view.py
+++ hiring_module/hiring_app/views/control_board/external_user_dashboard_view.py
@@ -1,10 +1,29 @@
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
-from hiring_app.views.control_board.utilities import role_redirect
+from functools import wraps
+from django.shortcuts import redirect
+
+def role_and_login_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('/')  # Redirect to home page if not authenticated
+        # Check if the user is already on the correct dashboard
+        elif request.user.groups.filter(name='admin').exists() and request.path != '/hiring_app/administrator_dashboard/':
+            return redirect('hiring_app:administrator_dashboard')
+        elif request.user.groups.filter(name='leader').exists() and request.path != '/hiring_app/leader_dashboard/':
+            return redirect('hiring_app:leader_dashboard')
+        elif request.user.groups.filter(name='manager').exists() and request.path != '/hiring_app/manager_dashboard/':
+            return redirect('hiring_app:manager_dashboard')
+        elif not request.user.groups.exists() and request.path != '/hiring_app/external_user_dashboard/':
+            return redirect('hiring_app:external_user_dashboard')
+        return view_func(request, *args, **kwargs)
+    return wrapper
 
 class ExternalUserDashboardView(TemplateView):
     template_name = 'external_user_dashboard.html'
-    @method_decorator(role_redirect)
+
+    @method_decorator(role_and_login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
