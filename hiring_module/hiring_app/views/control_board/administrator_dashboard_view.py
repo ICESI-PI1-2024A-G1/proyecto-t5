271ff9c62b1eeb8684365d901a0cde6961749f6d
Mendo6472 <mendo6472@gmail.com>
2024-03-30 22:17:03 -0500

    REF:BUILD: Modified way to display the user avatar in header

--- hiring_module/hiring_app/views/control_board/administrator_dashboard_view.py
+++ hiring_module/hiring_app/views/control_board/administrator_dashboard_view.py
@@ -16,16 +16,6 @@ class AdministratorDashboardView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Retrieve the first name and last name of the logged-in user
-        first_name = self.request.user.first_name
-        last_name = self.request.user.last_name
-        # Get the first word of the first name and last name
-        first_name = first_name.split()[0]
-        last_name = last_name.split()[0]
-        # Pass the first name and last name to the context
-        context['user_first_name'] = first_name
-        context['user_last_name'] = last_name
-        # Retrieve contract requests
         solicitudes_cex = CEXContractRequest.objects.all()
         solicitudes_monitoring = MonitoringContractRequest.objects.all()
         # Combine both queries into a single list
--- hiring_module/hiring_app/views/control_board/leader_dashboard_view.py
+++ hiring_module/hiring_app/views/control_board/leader_dashboard_view.py
@@ -10,15 +10,3 @@ class LeaderDashboardView(TemplateView):
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Retrieve the first name and last name of the logged-in user
-        first_name = self.request.user.first_name
-        last_name = self.request.user.last_name
-        # Get the first word of the first name and last name
-        first_name = first_name.split()[0]
-        last_name = last_name.split()[0]
-        # Pass the first name and last name to the context
-        context['user_first_name'] = first_name
-        context['user_last_name'] = last_name
-        return context
