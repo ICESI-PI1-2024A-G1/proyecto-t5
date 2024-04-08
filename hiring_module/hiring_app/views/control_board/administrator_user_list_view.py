73e364342b1db6a2fbc78ef9ad276caf0dcb7a4f
Mendo6472 <mendo6472@gmail.com>
2024-04-07 22:56:39 -0500

    WIP:BUILD: Added backend logic to handle the user list

---
+++ hiring_module/hiring_app/views/control_board/administrator_user_list_view.py
@@ -0,0 +1,56 @@
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.db.models import Q, CharField, Value
+from django.db.models.functions import Concat
+
+from hiring_app.model.user_model import CustomUser
+from .utilities import admin_required
+
+
+
+class AdministratorUserListView(TemplateView):
+    template_name = 'administrator_user_list.html'
+
+    # Redirect to correct dashboard based on user role
+    @method_decorator(admin_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        users = CustomUser.objects.all()
+        # Add a role field to each user object
+        for user in users:
+            user.role = str(user.groups.first())
+        context['users'] = users
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        search_query = request.POST.get('search_query')
+        if 'clear_search' in request.POST:
+            search_query = None
+
+        if 'search_by' in request.POST:
+            # If either search button is clicked, determine which type of search is requested
+            search_by = request.POST.get('search_by')
+            if search_by == 'email':
+                users = CustomUser.objects.filter(email__icontains=search_query)
+            elif search_by == 'name':
+                search_terms = search_query.split()
+                query = Q()
+                for term in search_terms:
+                    query &= (Q(first_name__icontains=term) | Q(last_name__icontains=term))
+                    # Filter users based on the query
+                users = CustomUser.objects.annotate(
+                    full_name=Concat('first_name', Value(' '), 'last_name', output_field=CharField())
+                ).filter(query)
+
+        else:
+            # If no search button is clicked, display all users
+            users = CustomUser.objects.all()
+
+        context = self.get_context_data()
+        context['users'] = users
+        return self.render_to_response(context)
+
--- hiring_module/hiring_app/views/control_board/utilities.py
+++ hiring_module/hiring_app/views/control_board/utilities.py
@@ -19,3 +19,48 @@ def role_redirect(view_func):
         return view_func(request, *args, **kwargs)
     
     return wrapper
+
+# Decorator to ensure that only users with the 'admin' role can access a view
+def admin_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.groups.filter(name='admin').exists():
+            if request.user.groups.filter(name='leader').exists():
+                return redirect('hiring_app:leader_dashboard')
+            elif request.user.groups.filter(name='manager').exists():
+                return redirect('hiring_app:manager_dashboard')
+            else:
+                return redirect('hiring_app:external_user_dashboard')
+        return view_func(request, *args, **kwargs)
+    
+    return wrapper
+
+# Decorator to ensure that only users with the 'leader' role can access a view
+def leader_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.groups.filter(name='leader').exists():
+            if request.user.groups.filter(name='admin').exists():
+                return redirect('hiring_app:administrator_dashboard')
+            elif request.user.groups.filter(name='manager').exists():
+                return redirect('hiring_app:manager_dashboard')
+            else:
+                return redirect('hiring_app:external_user_dashboard')
+        return view_func(request, *args, **kwargs)
+    
+    return wrapper
+
+# Decorator to ensure that only users with the 'manager' role can access a view
+def manager_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.groups.filter(name='manager').exists():
+            if request.user.groups.filter(name='admin').exists():
+                return redirect('hiring_app:administrator_dashboard')
+            elif request.user.groups.filter(name='leader').exists():
+                return redirect('hiring_app:leader_dashboard')
+            else:
+                return redirect('hiring_app:external_user_dashboard')
+        return view_func(request, *args, **kwargs)
+    
+    return wrapper
