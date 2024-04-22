2faf88c7ec23548a7c5b60e4cb5619ee4ff54753
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2024-04-22 09:51:46 -0500

    FIX: fixinh frontend

--- hiring_module/hiring_app/static/js/side_bar.js
+++ hiring_module/hiring_app/static/js/side_bar.js
@@ -1,12 +1,11 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById('toggleAside');
-    const sidebar = document.getElementById('asideMenu');
-    const mainContent = document.getElementById('main');
+  const toggleButton = document.getElementById("toggleAside");
+  const sidebar = document.getElementById("asideMenu");
+  const mainContent = document.getElementById("main");
 
-    toggleButton.addEventListener('click', function() {
-        sidebar.classList.toggle('hidden');
-        mainContent.classList.toggle('expanded');
-        mainContent.classList.toggle('nonExpanded');
-    });
+  toggleButton.addEventListener("click", function () {
+    sidebar.classList.toggle("hidden");
+    mainContent.classList.toggle("expanded");
+    mainContent.classList.toggle("nonExpanded");
+  });
 });
