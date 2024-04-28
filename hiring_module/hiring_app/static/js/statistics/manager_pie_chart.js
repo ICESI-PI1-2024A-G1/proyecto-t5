7f7cd08dc26c1b18bfb984b0a4d7a5cb2b25da26
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2024-04-28 10:56:44 -0500

    FIX: fixing interface to individual statistics

--- hiring_module/hiring_app/static/js/statistics/manager_pie_chart.js
+++ hiring_module/hiring_app/static/js/statistics/manager_pie_chart.js
@@ -1,37 +1,46 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+var managerChart;
 
-
-
-// Pie Chart Example
-var ctx = document.getElementById("manager_chart_pie");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Solicitudes Aprobadas", "Solicitudes Solicitudes En revision", "Solicitudes por validar"],
-    datasets: [{
-      data: [parseInt(document.getElementById("solicitudes_aprobadas_manager").innerText), parseInt(document.getElementById("solicitudes_en_revision_manager").innerText), parseInt(document.getElementById("solicitudes_por_validar_manager").innerText)],
-      backgroundColor: ['#1cc88a', '#f6c23e', '#e74a3b'],
-      hoverBackgroundColor: ['#188f64', '#c99f33', '#9d342a'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false
-    },
-    cutoutPercentage: 80,
-  },
-});
+function createOrUpdateChart() {
+    var chartCanvas = document.getElementById("manager_chart_pie");
+    
+    // Eliminar el gráfico anterior si existe
+    if (managerChart) {
+        managerChart.destroy();
+    }
+    
+    // Crear el nuevo gráfico
+    var ctx = chartCanvas.getContext('2d');
+    managerChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Solicitudes Aprobadas", "Solicitudes En revisión", "Solicitudes por validar"],
+            datasets: [{
+                data: [
+                    parseInt(document.getElementById("solicitudes_aprobadas_manager").innerText),
+                    parseInt(document.getElementById("solicitudes_en_revision_manager").innerText),
+                    parseInt(document.getElementById("solicitudes_por_validar_manager").innerText)
+                ],
+                backgroundColor: ['#1cc88a', '#f6c23e', '#e74a3b'],
+                hoverBackgroundColor: ['#188f64', '#c99f33', '#9d342a'],
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
+            legend: {
+                display: false
+            },
+            cutoutPercentage: 80,
+        },
+    });
+}
--- hiring_module/hiring_app/views/utilities.py
+++ hiring_module/hiring_app/views/utilities.py
@@ -7,6 +7,8 @@ from hiring_app.model.cex_contract_request_model import CEXContractRequest
 from hiring_app.model.monitoring_contract_request_model import MonitoringContractRequest
 
 # Decorator to redirect leaders and administrators to manager statistics page
+
+
 def leader_or_admin_redirect_to_manager_statistics(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
@@ -29,17 +31,54 @@ def get_metrics():
     approved_requests = 0
     review_requests = 0
     for_validate_requests = 0
+    managers = CustomUser.objects.filter(
+        groups=Group.objects.get(name='manager'))
+    data = []
+    best_to_worse_manager = []
 
-    approved_requests = CEXContractRequest.objects.filter(state='filed').count() + MonitoringContractRequest.objects.filter(state='filed').count()
-    review_requests = CEXContractRequest.objects.filter(state='review').count() + MonitoringContractRequest.objects.filter(state='review').count()
-    for_validate_requests = CEXContractRequest.objects.filter(state='pending').count() + MonitoringContractRequest.objects.filter(state='pending').count()
-    for_validate_requests += CEXContractRequest.objects.filter(state='incomplete').count() + MonitoringContractRequest.objects.filter(state='incomplete').count()
+    for manager in managers:
+        cex_requests = CEXContractRequest.objects.filter(
+            manager_assigned_to=manager)
+        
+        monitoring_requests = MonitoringContractRequest.objects.filter(
+            manager_assigned_to=manager)
+        
+        
+        approved_requests = cex_requests.filter(state='filed').count(
+        ) + monitoring_requests.filter(state='filed').count()
+        
+        
+        review_requests = cex_requests.filter(state='review').count(
+        ) + monitoring_requests.filter(state='review').count()
+        
+        
+        for_validate_requests = cex_requests.filter(state='pending').count(
+        ) + monitoring_requests.filter(state='pending').count()
+        
+        
+        for_validate_requests += cex_requests.filter(state='incomplete').count(
+        ) + monitoring_requests.filter(state='incomplete').count()
+        
+        
+        data.append([(manager.first_name + ' ' + manager.last_name), approved_requests,
+                    review_requests, for_validate_requests])
+        
+        
+        best_to_worse_manager.append([(manager.first_name + ' ' + manager.last_name), quality_calculator(approved_requests, review_requests, for_validate_requests)])
 
-    
-    
+    best_to_worse_manager = sorted(best_to_worse_manager, key=lambda x: x[1], reverse=True)
 
     return {
-        'approved_requests': approved_requests,
-        'review_requests': review_requests,
-        'for_validate_requests': for_validate_requests,
+        'data': data,
+        'quality': best_to_worse_manager
     }
+
+def quality_calculator(aprobadas, en_revision, por_validar):
+    total = aprobadas + en_revision + por_validar
+
+    if total != 0:
+        porcentaje_aprobadas = (aprobadas / total) * 100
+        porcentaje_aprobadas = round(porcentaje_aprobadas, 2)
+        return porcentaje_aprobadas
+    else:
+        return 0 
