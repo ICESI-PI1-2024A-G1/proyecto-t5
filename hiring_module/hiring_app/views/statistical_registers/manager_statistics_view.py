f79e0d7cb5af76fd1dd28c7a940105331dfa720b
TiagoVG <svalenciagarcia707@gmail.com>
2024-04-27 21:55:46 -0500

    WIP:BUILD: Added manager statistics and the best manager within the manager statistics page

--- hiring_module/hiring_app/views/statistical_registers/manager_statistics_view.py
+++ hiring_module/hiring_app/views/statistical_registers/manager_statistics_view.py
@@ -2,14 +2,16 @@ from django.views import View
 from django.views.generic import TemplateView
 from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
-from .utilities import leader_redirect_to_manager_statistics  # Importar el decorador
-
+from .utilities import leader_or_admin_redirect_to_manager_statistics, get_metrics
 
 
 class ManagerStatisticsView(View):
     template_name = 'statistical_registers/manager_statistics.html'
-    
-    @method_decorator(leader_redirect_to_manager_statistics)
+
+    @method_decorator(leader_or_admin_redirect_to_manager_statistics)
     def dispatch(self, request, *args, **kwargs):
-        # Aquí puedes agregar cualquier lógica adicional que necesites antes de mostrar la vista
-        return render(request, self.template_name)
+        # Obtener las métricas necesarias
+        metrics = get_metrics(request.user)
+
+        # Pasar las métricas como contexto a la plantilla
+        return render(request, self.template_name, metrics)
--- hiring_module/hiring_app/views/statistical_registers/utilities.py
+++ hiring_module/hiring_app/views/statistical_registers/utilities.py
@@ -1,16 +1,84 @@
 from functools import wraps
 from django.shortcuts import redirect
 from django.urls import reverse
+from hiring_app.model import CustomUser
+from django.contrib.auth.models import Group
+from hiring_app.model.cex_contract_request_model import CEXContractRequest
+from hiring_app.model.monitoring_contract_request_model import MonitoringContractRequest
 
 # Decorador para redirigir a los líderes a la página de estadísticas del manager
-def leader_redirect_to_manager_statistics(view_func):
+
+
+# Decorador para redirigir a los líderes y administradores a la página de estadísticas del manager
+def leader_or_admin_redirect_to_manager_statistics(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        if request.user.groups.filter(name='leader').exists():
-            # Si el usuario es un líder, lo redirigimos a la página de estadísticas del manager
-            return redirect('hiring_app:manager_statistics')
-        
-        # Si no es líder, llamamos a la vista original
+        if not request.user.is_authenticated:
+            # Si el usuario no está autenticado, redirigirlo al inicio de sesión
+            return redirect('login')
+
+        if request.user.groups.filter(name__in=['leader', 'admin']).exists():
+            # Si el usuario es líder o administrador, y no está en la página de estadísticas del manager, redirigirlo allí
+            if not request.path == reverse('hiring_app:manager_statistics'):
+                return redirect('hiring_app:manager_statistics')
+
+        # Si no es líder ni administrador, o ya está en la página de estadísticas del manager, llamar a la vista original
         return view_func(request, *args, **kwargs)
-    
+
     return wrapper
+
+
+def get_metrics(user):
+    # Obtener todos los gestores
+    group_manager = Group.objects.get(name='manager')
+    managers = CustomUser.objects.filter(groups=group_manager)
+
+    # Inicializar el contador de solicitudes
+    total_requests = 0
+    approved_requests = 0
+    review_requests = 0
+    for_validate_requests = 0
+    best_manager = None
+    max_approved_requests = 0
+    all_requests_assigned_to_the_best_manager = 0
+
+    # Contar las solicitudes asignadas a cada gestor
+    for manager in managers:
+        # Consultar las solicitudes asignadas a este gestor en CEXContractRequest
+        cex_requests = CEXContractRequest.objects.filter(
+            manager_assigned_to=manager)
+        # Consultar las solicitudes asignadas a este gestor en MonitoringContractRequest
+        monitoring_requests = MonitoringContractRequest.objects.filter(
+            manager_assigned_to=manager)
+
+        # Combinar los resultados de ambas consultas
+        total_requests_for_a_manager = cex_requests.count() + monitoring_requests.count()
+        approved_requests += cex_requests.filter(state='filed').count(
+        ) + monitoring_requests.filter(state='filed').count()
+        review_requests += cex_requests.filter(state='review').count(
+        ) + monitoring_requests.filter(state='review').count()
+        for_validate_requests += cex_requests.filter(state__in=['pending', 'incomplete']).count(
+        ) + monitoring_requests.filter(state__in=['pending', 'incomplete']).count()
+
+        total_requests += total_requests_for_a_manager
+
+        if approved_requests > max_approved_requests:
+            max_approved_requests = approved_requests
+            best_manager = manager.first_name + ' ' + manager.last_name
+            all_requests_assigned_to_the_best_manager = total_requests_for_a_manager
+
+    if best_manager:
+        effectiveness_percentage = (
+            max_approved_requests / all_requests_assigned_to_the_best_manager) * 100
+    else:
+        effectiveness_percentage = 0
+
+    # Devolver las métricas
+    return {
+        'total_requests': total_requests,
+        'approved_requests': approved_requests,
+        'review_requests': review_requests,
+        'for_validate_requests': for_validate_requests,
+        'best_manager': best_manager,
+        'effectiveness_percentage': effectiveness_percentage,
+    }
