0dfc1f6d2a2de6c3ff013790d64e691e3b7012c1
Mendo6472 <mendo6472@gmail.com>
2024-04-29 03:24:46 -0500

    END:BUILD: Added statistics for contract requests

--- hiring_module/hiring_app/views/utilities.py
+++ hiring_module/hiring_app/views/utilities.py
@@ -5,6 +5,9 @@ from hiring_app.model import CustomUser
 from django.contrib.auth.models import Group
 from hiring_app.model.cex_contract_request_model import CEXContractRequest
 from hiring_app.model.monitoring_contract_request_model import MonitoringContractRequest
+from hiring_app.model.provision_of_services_request_model import ProvisionOfServicesContractRequest
+from collections import defaultdict
+from datetime import datetime
 
 # Decorator to redirect leaders and administrators to manager statistics page
 
@@ -33,8 +36,31 @@ def get_metrics():
     for_validate_requests = 0
     managers = CustomUser.objects.filter(
         groups=Group.objects.get(name='manager'))
+    cex_requests_ammount = 0
+    monitoring_request_ammount = 0
+    pos_requests_ammount = 0
     data = []
     best_to_worse_manager = []
+    daily_requests = []
+    monthly_requests = []
+
+    cex_requests = CEXContractRequest.objects.all()
+    monitoring_requests = MonitoringContractRequest.objects.all()
+    pos_requests = ProvisionOfServicesContractRequest.objects.all()
+
+    # Initialize a defaultdict to store requests count for each date
+    requests_count_by_date = defaultdict(lambda: [0, 0, 0])
+
+    # Iterate over each type of request
+    for request_type, queryset in [("CEX", cex_requests), ("Monitoring", monitoring_requests), ("POS", pos_requests)]:
+        for request in queryset:
+            start_date = request.start_date
+            requests_count_by_date[start_date][{"CEX": 0, "Monitoring": 1, "POS": 2}[request_type]] += 1
+
+    # Convert the defaultdict to a list of lists
+    daily_requests = [[date.strftime('%Y-%m-%d')] + counts for date, counts in requests_count_by_date.items()]
+
+    daily_requests.sort()
 
     for manager in managers:
         cex_requests = CEXContractRequest.objects.filter(
@@ -43,21 +69,32 @@ def get_metrics():
         monitoring_requests = MonitoringContractRequest.objects.filter(
             manager_assigned_to=manager)
         
+        pos_requests = ProvisionOfServicesContractRequest.objects.filter(
+            manager_assigned_to=manager)
+        
         
         approved_requests = cex_requests.filter(state='filed').count(
-        ) + monitoring_requests.filter(state='filed').count()
+        ) + monitoring_requests.filter(state='filed').count(
+        ) + pos_requests.filter(state='filed').count(
+        )
         
         
         review_requests = cex_requests.filter(state='review').count(
-        ) + monitoring_requests.filter(state='review').count()
+        ) + monitoring_requests.filter(state='review').count(
+        ) + pos_requests.filter(state='review').count(
+        )
         
         
         for_validate_requests = cex_requests.filter(state='pending').count(
-        ) + monitoring_requests.filter(state='pending').count()
+        ) + monitoring_requests.filter(state='pending').count(
+        ) + pos_requests.filter(state='pending').count(
+        )
         
         
         for_validate_requests += cex_requests.filter(state='incomplete').count(
-        ) + monitoring_requests.filter(state='incomplete').count()
+        ) + monitoring_requests.filter(state='incomplete').count(
+        ) + pos_requests.filter(state='incomplete').count(
+        )
         
         
         data.append([(manager.first_name + ' ' + manager.last_name), approved_requests,
@@ -69,15 +106,16 @@ def get_metrics():
     best_to_worse_manager = sorted(best_to_worse_manager, key=lambda x: x[1], reverse=True)
 
     best_to_worse_manager = best_to_worse_manager[:5]
-    
-    
+
     best_to_worse_manager_with_percentage = [(nombre, f"{porcentaje} %") for nombre, porcentaje in best_to_worse_manager]
 
     
-
+    
     return {
         'data': data,
-        'quality': best_to_worse_manager_with_percentage
+        'quality': best_to_worse_manager_with_percentage,
+        'daily_requests': daily_requests,
+        'monthly_requests': monthly_requests
     }
 
 def quality_calculator(aprobadas, en_revision, por_validar):
