6398ceea604bf40f48716e030a27364960d34473
Mendo6472 <mendo6472@gmail.com>
2024-05-12 18:38:57 -0500

    REF:BUILD: Fixed not being able to download rut from pos request

--- hiring_module/hiring_app/views/request_creation/cex_contract_request_view.py
+++ hiring_module/hiring_app/views/request_creation/cex_contract_request_view.py
@@ -2,6 +2,7 @@ from django.http import Http404, HttpResponse
 from django.views.generic import CreateView
 from hiring_app.forms import CEXContractRequestForm
 from hiring_app.model import CEXContractRequest
+from hiring_app.model import ProvisionOfServicesContractRequest
 from django.urls import reverse_lazy
 from datetime import datetime, timedelta
 from .utilities import utilities
@@ -45,11 +46,16 @@ class CEXContractRequestView(CreateView):
 
 def download_rut_file(request, idContract, *args, **kwargs):
     # Get the rut and send it as a file
-    model_instance = get_object_or_404(CEXContractRequest, id=idContract)
+    model_instance = None
+    
+    if CEXContractRequest.objects.filter(id=idContract).exists():
+        model_instance = get_object_or_404(CEXContractRequest, id=idContract)
+    else:
+        model_instance = get_object_or_404(ProvisionOfServicesContractRequest, id=idContract)
     if not request.user.has_perm('your_app.view_cexcontractrequest'):
         raise PermissionDenied(
             "You don't have permission to download this file.")
-
+        
     if not model_instance.rut:
         raise Http404("The requested file does not exist.")
     try:
