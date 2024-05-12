47f1b0d609a7e9a12713529714cc251ebb8b27d9
Alejolonber25 <alejandrolonber25@gmail.com>
2024-05-12 16:12:23 -0500

    END:BUILD: Export requests to excel implemented

--- hiring_module/hiring_app/views/control_board/utilities.py
+++ hiring_module/hiring_app/views/control_board/utilities.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import Group
 from hiring_app.model.cex_contract_request_model import CEXContractRequest
 from hiring_app.model.monitoring_contract_request_model import MonitoringContractRequest
 from hiring_app.model.provision_of_services_request_model import ProvisionOfServicesContractRequest
+from django.http import HttpResponse
+from openpyxl import Workbook
 
 # Decorator to redirect users to the correct dashboard based on their role
 def role_redirect(view_func):
@@ -105,4 +107,257 @@ def get_requests(user):
 
         
         
-        
+def export_requests(user):
+    groups = [group.name for group in user.groups.all()]
+    requests_CEX = CEXContractRequest.objects.none()
+    requests_monitoring = MonitoringContractRequest.objects.none()
+    requests_pos = ProvisionOfServicesContractRequest.objects.none()
+
+    if 'admin' in groups:
+        requests_CEX = CEXContractRequest.objects.all()
+        requests_monitoring = MonitoringContractRequest.objects.all()
+        requests_pos = ProvisionOfServicesContractRequest.objects.all()
+    elif 'leader' in groups:
+        requests_CEX = CEXContractRequest.objects.filter(leader_assigned_to=user.id)
+        requests_monitoring = MonitoringContractRequest.objects.filter(leader_assigned_to=user.id)
+        requests_pos = ProvisionOfServicesContractRequest.objects.filter(leader_assigned_to=user.id)
+    elif 'manager' in groups:
+        requests_CEX = CEXContractRequest.objects.filter(manager_assigned_to=user.id)
+        requests_monitoring = MonitoringContractRequest.objects.filter(manager_assigned_to=user.id)
+        requests_pos = ProvisionOfServicesContractRequest.objects.filter(manager_assigned_to=user.id)
+
+    
+
+    wb = Workbook()
+
+    ws1 = wb.active
+    ws1.title = "Solicitudes de Contratación"
+    
+    headers = [
+    "ID",
+    "Start Date",
+    "Completion Date",
+    "Estimated Completion Date",
+    "Current State Start",
+    "State",
+    "Manager Assigned To",
+    "Leader Assigned To",
+    "Created By",
+    ]
+    
+    ws1.append(headers)
+    
+    for request in (list(requests_CEX) + list(requests_monitoring) + list(requests_pos)):
+        row_data = [
+            str(request.id),
+            request.start_date.strftime('%Y-%m-%d'),
+            request.completion_date.strftime('%Y-%m-%d %H:%M:%S') if request.completion_date else None,
+            request.estimated_completion_date.strftime('%Y-%m-%d') if request.estimated_completion_date else None,
+            request.current_state_start.strftime('%Y-%m-%d %H:%M:%S'),
+            request.state,
+            f'{request.manager_assigned_to.first_name} {request.manager_assigned_to.last_name}' if request.manager_assigned_to else 'sin asignar',
+            f'{request.leader_assigned_to.first_name} {request.leader_assigned_to.last_name}' if request.leader_assigned_to else 'sin asignar',
+            f'{request.created_by.first_name} {request.created_by.last_name}' if request.created_by else 'sin asignar',
+        ]
+        ws1.append(row_data)
+    
+    
+    ws2 = wb.create_sheet(title="CEX")
+    
+    headers = [
+    "ID",
+    "Start Date",
+    "Completion Date",
+    "Estimated Completion Date",
+    "Current State Start",
+    "State",
+    "Manager Assigned To",
+    "Leader Assigned To",
+    "Created By",
+    "Hiree Full Name",
+    "Hiree ID",
+    "Hiree Cellphone",
+    "Hiree Email",
+    "Cenco",
+    "Request Motive",
+    "Banking Entity",
+    "Bank Account Type",
+    "Bank Account Number",
+    "EPS",
+    "Pension Fund",
+    "ARL",
+    "Contract Value",
+    "Charge Account",
+    "RUT"
+    ]
+    ws2.append(headers)
+    
+    for cex_request in requests_CEX:
+        row_data = [
+            str(cex_request.id),
+            cex_request.start_date.strftime('%Y-%m-%d'),
+            cex_request.completion_date.strftime('%Y-%m-%d %H:%M:%S') if cex_request.completion_date else None,
+            cex_request.estimated_completion_date.strftime('%Y-%m-%d') if cex_request.estimated_completion_date else None,
+            cex_request.current_state_start.strftime('%Y-%m-%d %H:%M:%S'),
+            cex_request.state,
+            f'{cex_request.manager_assigned_to.first_name} {cex_request.manager_assigned_to.last_name}' if cex_request.manager_assigned_to else 'sin asignar',
+            f'{cex_request.leader_assigned_to.first_name} {cex_request.leader_assigned_to.last_name}' if cex_request.leader_assigned_to else 'sin asignar',
+            f'{cex_request.created_by.first_name} {cex_request.created_by.last_name}' if cex_request.created_by else 'sin asignar',
+            cex_request.hiree_full_name,
+            cex_request.hiree_id,
+            cex_request.hiree_cellphone,
+            cex_request.hiree_email,
+            cex_request.cenco,
+            cex_request.request_motive,
+            cex_request.banking_entity,
+            cex_request.bank_account_type,
+            cex_request.bank_account_number,
+            cex_request.eps,
+            cex_request.pension_fund,
+            cex_request.arl,
+            cex_request.contract_value,
+            cex_request.charge_account,
+            cex_request.rut.url if cex_request.rut else ""
+        ]
+        ws2.append(row_data)
+
+    ws3 = wb.create_sheet(title="Monitorías")
+    
+    headers = [
+    "ID",
+    "Start Date",
+    "Completion Date",
+    "Estimated Completion Date",
+    "Current State Start",
+    "State",
+    "Manager Assigned To",
+    "Leader Assigned To",
+    "Created By",
+    "Cenco",
+    "Has Money in Cenco",
+    "Cenco Manager",
+    "Monitoring Type",
+    "Student Code",
+    "Student Full Name",
+    "Student ID",
+    "Student Email",
+    "Student Cellphone",
+    "Daviplata",
+    "Course or Project",
+    "Monitoring Description",
+    "Weekly Hours",
+    "Total Value to Pay",
+    "Is Unique Payment"
+    ]
+    ws3.append(headers)
+
+    for monitoring_request in requests_monitoring:
+        row_data = [
+            str(monitoring_request.id),
+            monitoring_request.start_date.strftime('%Y-%m-%d'),
+            monitoring_request.completion_date.strftime('%Y-%m-%d %H:%M:%S') if monitoring_request.completion_date else None,
+            monitoring_request.estimated_completion_date.strftime('%Y-%m-%d') if monitoring_request.estimated_completion_date else None,
+            monitoring_request.current_state_start.strftime('%Y-%m-%d %H:%M:%S'),
+            monitoring_request.state,
+            f'{monitoring_request.manager_assigned_to.first_name} {monitoring_request.manager_assigned_to.last_name}' if monitoring_request.manager_assigned_to else 'sin asignar',
+            f'{monitoring_request.leader_assigned_to.first_name} {monitoring_request.leader_assigned_to.last_name}' if monitoring_request.leader_assigned_to else 'sin asignar',
+            f'{monitoring_request.created_by.first_name} {monitoring_request.created_by.last_name}' if monitoring_request.created_by else 'sin asignar',
+            monitoring_request.cenco,
+            monitoring_request.has_money_in_cenco,
+            monitoring_request.cenco_manager,
+            monitoring_request.monitoring_type,
+            monitoring_request.student_code,
+            monitoring_request.student_full_name,
+            monitoring_request.student_id,
+            monitoring_request.student_email,
+            monitoring_request.student_cellphone,
+            monitoring_request.daviplata,
+            monitoring_request.course_or_proyect,
+            monitoring_request.monitoring_description,
+            monitoring_request.weekly_hours,
+            monitoring_request.total_value_to_pay,
+            monitoring_request.is_unique_payment
+        ]
+        ws3.append(row_data)
+
+    ws4 = wb.create_sheet(title="Honorarios")
+    
+    headers = [
+    "ID",
+    "Start Date",
+    "Completion Date",
+    "Estimated Completion Date",
+    "Current State Start",
+    "State",
+    "Manager Assigned To",
+    "Leader Assigned To",
+    "Created By",
+    "Hiree Full Name",
+    "Hiree ID",
+    "Hiree Cellphone",
+    "Hiree Email",
+    "Cenco",
+    "Request Motive",
+    "Banking Entity",
+    "Bank Account Type",
+    "Bank Account Number",
+    "EPS",
+    "Pension Fund",
+    "ARL",
+    "Contract Value",
+    "Charge Account",
+    "RUT",
+    "Course Name",
+    "Period",
+    "Group",
+    "Intensity",
+    "Total Hours",
+    "Course Code",
+    "Students Quantity",
+    "Additional Hours"
+    ]
+
+    ws4.append(headers)
+    
+    for post_request in requests_pos:
+        row_data = [
+            str(post_request.id),
+            post_request.start_date.strftime('%Y-%m-%d'),
+            post_request.completion_date.strftime('%Y-%m-%d %H:%M:%S') if post_request.completion_date else None,
+            post_request.estimated_completion_date.strftime('%Y-%m-%d') if post_request.estimated_completion_date else None,
+            post_request.current_state_start.strftime('%Y-%m-%d %H:%M:%S'),
+            post_request.state,
+            f'{post_request.manager_assigned_to.first_name} {post_request.manager_assigned_to.last_name}' if post_request.manager_assigned_to else 'sin asignar',
+            f'{post_request.leader_assigned_to.first_name} {post_request.leader_assigned_to.last_name}' if post_request.leader_assigned_to else 'sin asignar',
+            f'{post_request.created_by.first_name} {post_request.created_by.last_name}' if post_request.created_by else 'sin asignar',
+            post_request.hiree_full_name,
+            post_request.hiree_id,
+            post_request.hiree_cellphone,
+            post_request.hiree_email,
+            post_request.cenco,
+            post_request.request_motive,
+            post_request.banking_entity,
+            post_request.bank_account_type,
+            post_request.bank_account_number,
+            post_request.eps,
+            post_request.pension_fund,
+            post_request.arl,
+            post_request.contract_value,
+            post_request.charge_account,
+            post_request.rut.url if post_request.rut else "", 
+            post_request.course_name,
+            post_request.period,
+            post_request.group,
+            post_request.intensity,
+            post_request.total_hours,
+            post_request.course_code,
+            post_request.students_quantity,
+            post_request.additional_hours
+        ]
+        ws4.append(row_data)
+
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="solicitudes.xlsx"'
+    wb.save(response)
+
+    return response
