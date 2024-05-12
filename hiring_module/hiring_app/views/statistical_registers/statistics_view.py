f7177801d3ad28c9a0b2ec97f57004a7499edee6
Santiago Valencia <svalenciagarcia707@gmail.com>
2024-05-12 13:47:36 -0500

    END:BUG: The imports are deleted

--- hiring_module/hiring_app/views/statistical_registers/statistics_view.py
+++ hiring_module/hiring_app/views/statistical_registers/statistics_view.py
@@ -2,9 +2,7 @@ from django.views import View
 from django.views.generic import TemplateView
 from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
-from django.http import JsonResponse
-from django.urls import reverse
-from ..utilities import leader_or_admin_redirect_to_manager_statistics, get_manager_metrics, get_leader_metrics, get_average_duration, filter_quantity_of_requests_by_date_range
+from ..utilities import leader_or_admin_redirect_to_manager_statistics, get_manager_metrics, get_leader_metrics, get_average_duration
 
 
 class StatisticsView(View):
