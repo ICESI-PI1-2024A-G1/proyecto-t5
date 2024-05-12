43982c638488741769c8ec702f01991d0570a2b8
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2024-05-12 13:08:15 -0500

    FIX: fixing views and model

--- hiring_module/hiring_app/views/statistical_registers/statistics_view.py
+++ hiring_module/hiring_app/views/statistical_registers/statistics_view.py
@@ -7,38 +7,19 @@ from django.urls import reverse
 from ..utilities import leader_or_admin_redirect_to_manager_statistics, get_manager_metrics, get_leader_metrics, get_average_duration, filter_quantity_of_requests_by_date_range
 
 
-
 class StatisticsView(View):
     template_name = 'statistical_registers/statistics.html'
 
-    #@method_decorator(leader_or_admin_redirect_to_manager_statistics)
+    # @method_decorator(leader_or_admin_redirect_to_manager_statistics)
     def dispatch(self, request, *args, **kwargs):
         # Get the required metrics
 
-        print(reverse('statistics'))
         manager_metrics = get_manager_metrics()
         leader_metrics = get_leader_metrics()
         average_duration = get_average_duration()
         group = {'actualgroup': 'other'}
-        user_group = self.request.user.groups.first()
-        if user_group:
-            if user_group.name == 'admin':
-                group = {'actualgroup': 'admin'}
-        # if self.request.user.groups.first().name == 'admin':
-        #     group = {'actualgroup': 'admin'}
-        context = {
-            'manager_metrics': manager_metrics,
-            'leader_metrics': leader_metrics,
-            'average_duration': average_duration,
-            'statistics_url': reverse('statistics'),
-            'group': group
-        }
-        
+        if self.request.user.groups.first().name == 'admin':
+            group = {'actualgroup': 'admin'}
+
         # Pass metrics as context to template
-        return render(request, self.template_name, context)
-    
-    def post(self, request, *args, **kwargs):
-        start_date = request.POST.get('start_date')
-        end_date = request.POST.get('end_date')
-        total_requests_count = filter_quantity_of_requests_by_date_range(start_date, end_date)
-        return JsonResponse(total_requests_count)
+        return render(request, self.template_name, manager_metrics | leader_metrics | average_duration | group)
