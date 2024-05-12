ce8a4c16f0ccdd2f57fc0cc5f536b954aa9e1eec
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2024-05-12 00:23:34 -0500

    REF: fixing ui and improving the gui

--- hiring_module/hiring_app/views/request_creation/cex_contract_request_view.py
+++ hiring_module/hiring_app/views/request_creation/cex_contract_request_view.py
@@ -8,6 +8,7 @@ from .utilities import utilities
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
 
+
 class CEXContractRequestView(CreateView):
     model = CEXContractRequest
     form_class = CEXContractRequestForm
@@ -20,6 +21,7 @@ class CEXContractRequestView(CreateView):
         estimated_completion_date = datetime.now() + timedelta(days=30)
         leader = utilities.findLeaderToAssign()
         manager = utilities.findManagerToAssign()
+        form.instance.id_solicitant_name = self.request.user.first_name
         form.instance.estimated_completion_date = estimated_completion_date
         form.instance.created_by = current_user
         form.instance.leader_assigned_to = leader
@@ -29,9 +31,9 @@ class CEXContractRequestView(CreateView):
         cex_contract_request.create_snapshot()
         cex_contract_request.save()
         utilities.send_email('Solicitud de contratación radicada', 'Estimado/a, su solicitud de contrato en el aplicativo del módulo de contratación de la unidad de servicios compartidos ha sido radicada satisfactoriamente',
-                   current_user.email)
+                             current_user.email)
         return super().form_valid(form)
-    
+
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
 
@@ -39,14 +41,14 @@ class CEXContractRequestView(CreateView):
         context = super().get_context_data(**kwargs)
         context['actualgroup'] = 'external'
         return context
-    
 
-    
+
 def download_rut_file(request, idContract, *args, **kwargs):
     # Get the rut and send it as a file
     model_instance = get_object_or_404(CEXContractRequest, id=idContract)
     if not request.user.has_perm('your_app.view_cexcontractrequest'):
-        raise PermissionDenied("You don't have permission to download this file.")
+        raise PermissionDenied(
+            "You don't have permission to download this file.")
 
     if not model_instance.rut:
         raise Http404("The requested file does not exist.")
@@ -55,6 +57,8 @@ def download_rut_file(request, idContract, *args, **kwargs):
     except IOError as e:
         raise IOError(f"Error reading file: {e}")
 
-    response = HttpResponse(rut_file_data, content_type='application/octet-stream')
-    response['Content-Disposition'] = f'attachment; filename="{model_instance.rut.name}"'
+    response = HttpResponse(
+        rut_file_data, content_type='application/octet-stream')
+    response['Content-Disposition'] = f'attachment; filename="{
+        model_instance.rut.name}"'
     return response
